dccfe5a8e431f55f63e20530172c7bf9144c2e18
Atticus Liu <atticusacl@gmail.com>
2021-04-19 13:05:27 -0700

    update auth.py with globals

--- auth.py
+++ auth.py
@@ -2,6 +2,8 @@ import api_calls
 
 # this file holds all things related to authentication
 game_id_str = 'game_id'
+echo_response_str = "OK"
+echo_auth_response_str = "Auth OK"
 
 # setup and go through auth
 # RETURNS: api_key, game_id tuple
@@ -12,12 +14,12 @@ def set_up_and_auth_new_game() -> tuple:
     # verify echo calls
     echo_response = api_calls.get_echo(api_key).text
 
-    if echo_response != "OK":
+    if echo_response != echo_response_str:
         raise ValueError("Echo response is not OK. Quitting.")
 
     echo_auth_response = api_calls.get_echo_auth(api_key).text
 
-    if echo_auth_response != "Auth OK":
+    if echo_auth_response != echo_auth_response_str:
         raise ValueError("Echo auth response is not Auth OK. Quitting.")
 
     # create a new game and call .json() on it
